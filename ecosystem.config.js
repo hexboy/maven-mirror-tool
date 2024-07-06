d325bc50e414579279231157beb37b62316f949f
hexboy <hexboy2011@gmail.com>
2024-07-06 23:45:08 +0330

    add port env support

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'mmt-server',
-      script: 'tsx src/index.ts',
+      script: 'tsx src/index.ts --port=${PORT}',
       max_memory_restart: '300M',
+      env_production: {
+        NODE_ENV: "production"
+     },
+     env_development: {
+        NODE_ENV: "development"
+     }
     },
   ],
 };
